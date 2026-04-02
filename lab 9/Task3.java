49fad1b96b08fb6b765686ceddd8a58f311a475a
Azat077 <themasterkill0506@gmail.com>
2026-04-02 12:29:17 +0500

    Lab 9

---
+++ lab 9/Task3.java
@@ -0,0 +1,16 @@
+
+class MathUtils {
+    static int square(int x) {
+        return x * x;
+    }
+
+    static int cube(int x) {
+        return x * x * x;
+    }
+}
+
+public class Task3 {
+    public static void main(String[] args) {
+        System.out.println(MathUtils.square(3));
+    }
+}
